46ea7b01c734276f8c1831adf9607d51386cf50f
norkos <norbert.kosmal@gmail.com>
2022-10-10 19:56:38 +0200

    Vulnerability solved in checking email + changes in docker for consumer

---
+++ consumer/main.py
@@ -0,0 +1,33 @@
+import json
+import pika
+import os
+import time
+
+from fastapi import FastAPI
+
+
+app = FastAPI()
+messages = []
+PORT = os.environ.get('PORT')
+
+params = pika.URLParameters(os.getenv('CLOUDAMQP_URL'))
+connection = pika.BlockingConnection(params)
+channel = connection.channel()
+channel.queue_declare(queue='main', durable=True)
+
+
+def callback(ch, method, properties, body):
+    print('Received in main')
+    data = json.loads(body)
+    print(data)
+    time.sleep(20)
+    print('Done')
+    ch.basic_ack(delivery_tag=method.delivery_tag)
+
+
+channel.basic_qos(prefetch_count=1)
+channel.basic_consume(queue='main', on_message_callback=callback)
+print('Started consuming')
+
+channel.start_consuming()
+connection.close()
--- src/acm_service/utils/email_checker.py
+++ src/acm_service/utils/email_checker.py
@@ -2,5 +2,8 @@ import re
 regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
 
 
-def check(email: str):
-    return re.fullmatch(regex, email)
+def check(email: str) -> bool:
+    try:
+        return re.fullmatch(regex, email)
+    except:
+        return False
