bbca2e3b6030e12407b565ecabb99d47f1b7c859
norkos <norbert.kosmal@gmail.com>
2022-12-26 15:38:12 +0100

    Increasing abount of connections in the DB pool

--- src/acm_service/data_base/database.py
+++ src/acm_service/data_base/database.py
@@ -2,7 +2,7 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from acm_service.utils.env import ASYNC_DB_URL
 
-engine = create_async_engine(ASYNC_DB_URL, future=True, echo=False)
+engine = create_async_engine(ASYNC_DB_URL, future=True, echo=False, pool_size=10, max_overflow=20)
 async_session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession, autocommit=False, autoflush=False)
 
 Base = declarative_base()
